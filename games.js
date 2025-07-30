7ae3d9562da38967aaa5c4c0919641c6a09fcce4
OdetMorinaa <140353425+OdetMorinaa@users.noreply.github.com>
2025-07-30 12:12:23 -0700

    Delete games.js

--- games.js
+++
@@ -1,9 +0,0 @@
-import supabase from '../../lib/supabase';
-
-export default async function handler(req, res) {
-  const { data, error } = await supabase.from('games').select('*');
-
-  if (error) return res.status(500).json({ error: error.message });
-
-  res.status(200).json(data);
-}
